11d9dc2d70c74ad8aa963531a1f6ad6d648019c6
Pranav Bokey <pbokey@gmail.com>
2019-08-27 10:21:18 -0400

    added login button to main view and added login view and controller

--- Android/app/src/main/java/com/example/brewhaha_android/MainActivity.kt
+++ Android/app/src/main/java/com/example/brewhaha_android/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.brewhaha_android
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun showLogin(view: View): Unit{
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
