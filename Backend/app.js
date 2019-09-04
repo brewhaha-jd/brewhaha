c34d49113a1360215b641b40c9d7ae8c5894fd89
nickhutchinson <nickhutchinson@me.com>
2019-09-04 13:53:54 -0400

    #29 logging is now much better

--- Backend/app.js
+++ Backend/app.js
@@ -18,6 +18,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
 app.use(logger('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
